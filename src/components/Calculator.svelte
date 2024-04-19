6c4ca3f05eee1b69fbd4d001671776c13e3a05c8
Ales Zima <ales@cpex.cz>
2024-04-19 15:50:58 +0200

    Added screen UI

--- src/components/Calculator.svelte
+++ src/components/Calculator.svelte
@@ -1,12 +1,15 @@
 <script>
   import ThemeToggle from '../components/ThemeToggle.svelte';
+  import Keypad from './Keypad.svelte';
 </script>
 
 <body class="bg-main-bg text-base text-light-text font-bold">
-  <main class="p-8">
+  <main class="p-8 space-y-8">
     <div class="flex justify-between items-center">
       <h1>calc</h1>
       <ThemeToggle />
     </div>
+    <p class="text-end bg-calc-screen p-4 rounded-md">399,981</p>
+    <Keypad />
   </main>
 </body>
